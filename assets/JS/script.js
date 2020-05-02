1f64fe3a17bc9f32a875d8beba14ed82a7f72106
Cristian <duran.romero.cris@gmail.com>
2020-05-02 00:44:06 -0400

    Mecanicas y secciones hechas, FALTA DISEÑO!

---
+++ assets/JS/script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var salto = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(salto).offset().top
+        }, 800, function(){
+          window.location.hash = salto;
+        });
+      }
+    });
+  
+    $('[data-toggle="popover"]').popover();
+  
+  });
+  
